65f9eced8a32edc6af4e7bf98949a89f30e1b817
Ziad Hatem <ziadhatem2022@gmail.com>
2024-10-26 13:19:12 +0300

    some styles

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -1,19 +1,26 @@
 "use client";
 import VideoComponent from "@/app/[locale]/common/video-component";
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
 import React, { Suspense } from "react";
 
 const Features = ({ index }) => {
   const t = useTranslations("hero");
+  const lang = useLocale();
   const i = index + 1;
 
   return (
     <div className="w-[50%] lg:w-[25%] max-sm:h-[150px] flex justify-between items-end">
       <div className="w-[1px] h-[90%] bg-[#fff] bg-opacity-15" />
       <div className="mb-[40px]">
-        <h1 className="text-[20px] md:text-[calc(2vw_+_30px)] text-center text-white">
+        <h1
+          className={`text-[35px] ${
+            lang == "ar"
+              ? "text-[20px] md:text-[calc(1vw_+_25px)] mb-4"
+              : "text-[35px] md:text-[calc(2vw_+_30px)]"
+          } text-center text-white`}
+        >
           {t(`title${i}`)}
         </h1>
         <p className="max-sm:text-[10px] max-sm:h-[40px] max-sm:w-[95%] text-[14px] md:text-[12px] lg:text-[16px] text-white text-center">
